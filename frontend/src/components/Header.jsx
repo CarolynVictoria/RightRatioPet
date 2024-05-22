a7e87466c291aef05c9478b1d14793d38b25e5ff
cvmgo <design@newlightarts.com>
2024-05-22 19:15:10 -0400

    StyleUpdates

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,7 @@ const header = () => {
 			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
 				<Container>
 					<Navbar.Brand href='/' className='navtitle'>
-						<img src={logo} className='logo' />
+						<img src={logo} alt='right:ratio' className='logo' />
 						Right:Ratio Data Center
 					</Navbar.Brand>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
