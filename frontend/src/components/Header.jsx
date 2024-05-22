69611ad3bc6a5197ebb1773942051a6cd8258710
cvmgo <design@newlightarts.com>
2024-05-22 19:10:16 -0400

    Header Adjustments

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 const header = () => {
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
 				<Container>
-					<Navbar.Brand href='/'>Right:Ratio Data App</Navbar.Brand>
+					<Navbar.Brand href='/' className='navtitle'>
+						<img src={logo} className='logo' />
+						Right:Ratio Data Center
+					</Navbar.Brand>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
