232b5d802a8f8823224dd767f64631507e0a3f13
cvmgo <design@newlightarts.com>
2024-05-29 21:43:16 -0400

    serverjs-cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import clientRoutes from './routes/clientRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -23,17 +24,19 @@ app.use(cookieParser());
 app.use('/api/clients', clientRoutes);
 app.use('/api/users', userRoutes);
 
-if (process.env.NODE_ENV ==='production') {
+if (process.env.NODE_ENV === 'production') {
 	//	Set static folder
+	const __dirname = path.resolve();
 	app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 	//	Any route that is not api	will be redirected to index.html
 	app.get('*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 	);
-} else{
+} else {
+	const __dirname = path.resolve();
 	app.get('/', (req, res) => {
-		res.send('API is running...');
+		res.send('API is running....');
 	});
 }
 
